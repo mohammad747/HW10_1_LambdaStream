2bd15d21e69d460d27a85264a3d975ecff9079bf
Mohammad747 <mohammad.hashemi747@gamil.com>
2019-05-30 12:11:13 +0430

    Implementing the question

--- src/ir/maktabsharif/Main.java
+++ src/ir/maktabsharif/Main.java
@@ -1,8 +1,36 @@
 package ir.maktabsharif;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+/**
+ * @author Mohammad hashemi
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Arrays of names
+        List<String> testList = Arrays.asList("Amir" , "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+
+        // part a
+        Map<Integer, List<String>> mapStream = testList.stream()
+                .collect(groupingBy(String::length));
+
+
+        // part b
+        Map<Integer, Long> anotherMap = testList.stream()
+                .collect(groupingBy(String::length, counting()));
+
+
+        System.out.println(mapStream);
+
+        System.out.println(anotherMap);
+
+
     }
 }
